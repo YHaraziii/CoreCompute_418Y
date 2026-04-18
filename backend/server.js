d0260e90a40e7582a5ac17fe53ce0e115852c503
YHaraziii <yosef.haraziii@gmail.com>
2026-04-18 11:10:28 -0400

    server edits

--- backend/server.js
+++ backend/server.js
@@ -6,3 +6,12 @@ app.use(express.json());
 app.use(cors());
 
 app.listen(9000, ()=> { console.log('Server Started at ${9000}')})
+
+const mongoose = require('mongoose');
+const mongoString = "mongodb+srv://corecompute418:Core%40418@cluster0.jym5xk0.mongodb.net/CoreCompute?retryWrites=true&w=majority";
+mongoose.connect(mongoString)
+const database = mongoose.connection
+
+database.on('error', (error) => console.log(error))
+database.once('connected', () => console.log('Databased Connected'))
+
